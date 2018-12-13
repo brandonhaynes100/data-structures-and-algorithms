20926bb4e75168ead44eb5f2631f3865affc7f03
Brandon Haynes <brandon@Brandons-MBP-2.localdomain>
2018-12-12 21:07:38 -0800

    adding repeated_word.jpg to assessts

--- repeatedWord/repeated-word.js
+++ repeatedWord/repeated-word.js
@@ -1,8 +1,8 @@
 
 function repeatedWord(string) {
   let wordArray = string.split(' ');
-  for(let i = 0 ; i < string.length ; i++) {
-    for(let j = i ; j < string.length ; j++) {
+  for(let i = 0 ; i < string.length - 1 ; i++) {
+    for(let j = i + 1 ; j < string.length ; j++) {
       if(wordArray[i] == wordArray[j]) {
         return wordArray[i];
       }
